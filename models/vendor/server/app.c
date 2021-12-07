c822ec212ded60b6bd0238faaa736bdc21cdb95a
Sijin Woo <sijin.woo@silabs.com>
2021-12-07 13:39:25 -0600

    vendor models - adds license

--- models/vendor/server/app.c
+++ models/vendor/server/app.c
@@ -1,6 +1,6 @@
 /***************************************************************************//**
- * @file
- * @brief Core application logic.
+ * @file app.c
+ * @brief Core application logic for the vendor server node.
  *******************************************************************************
  * # License
  * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
@@ -26,6 +26,12 @@
  *    misrepresented as being the original software.
  * 3. This notice may not be removed or altered from any source distribution.
  *
+ ******************************************************************************
+ * # Experimental Quality
+ * This code has not been formally tested and is provided as-is. It is not
+ * suitable for production environments. In addition, this code will not be
+ * maintained and there may be no bug maintenance planned for these resources.
+ * Silicon Labs may update projects from time to time.
  ******************************************************************************/
 #include "em_common.h"
 #include "app_assert.h"
